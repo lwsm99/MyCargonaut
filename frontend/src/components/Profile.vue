6c84c6f1d53a5435a61d8e438ecf5705218aa9eb
Maxim <maxim.odenweller@mni.thm.de>
2023-01-01 11:25:50 +0100

    fix up profile, bug when going into <600px width?

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -1,16 +1,14 @@
 <template>
-    <v-container align="center">
+    <v-container id="main-container" align="center">
         <v-card class="profile-container" rounded="4" color="whitesmoke">
             <v-row justify="center" align="center">
-                <v-col cols="6">
-                    <div style="display: flex; justify-content: center; align-items: center;" class="profile-pic-name">
+                <v-col class="profile-pic-name" lg="6" sm="12" xs="12">
                         <v-avatar size="200px" class="ma-8">
                             <v-img src="https://cdn.vuetifyjs.com/images/lists/1.jpg"></v-img>
                         </v-avatar>
                         <h1 style="margin-left: 10px">Mark Mustermann</h1>
-                    </div>
                 </v-col>
-                <v-col cols="6">
+                <v-col lg="6" sm="12" xs="12">
                     <v-icon class="mx-2" icon="fas fa-star" color="orange" size="large"/>
                     <v-icon class="mx-2" icon="fas fa-star" color="orange" size="large"/>
                     <v-icon class="mx-2" icon="fas fa-star" color="orange" size="large"/>
@@ -19,27 +17,12 @@
                 </v-col>
             </v-row>
             <v-row class="bottom-row">
-                <v-col cols="6">
-                    <v-card class="ma-4" color="grey" rounded="4" width="90%" elevation="0">
-                        <v-card-title class="my-4">Meine Kommentare</v-card-title>
+                <v-col lg="6" sm="12" xs="12">
+                    <v-card class="ma-4" color="grey" rounded="4" width="90%" elevation="0" justify="center" align="center">
+                        <h2 class="my-4">Meine Kommentare</h2>
                         <!-- TODO: as many rows as comments -->
-                        <v-row class="mx-4 mb-4" align="center">
-                            <v-col cols="2">
-                                <v-avatar size="60px">
-                                    <v-img src="https://cdn.vuetifyjs.com/images/lists/1.jpg"></v-img>
-                                </v-avatar>
-                            </v-col>
-                            <v-col cols="10">
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut
-                                    aliquam aliquam, nunc nisl aliquet nisl, eget aliquet nisl nunc in elit. Sed
-                                    euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquet nisl nunc
-                                    in elit.
-                                </p>
-                            </v-col>
-                        </v-row>
-                        <v-row class="mx-4 mb-4" align="center">
-                            <v-col cols="2">
+                        <v-row class="mx-4 mb-4" align="center" justify="center">
+                            <v-col lg="2">
                                 <v-avatar size="60px">
                                     <v-img src="https://cdn.vuetifyjs.com/images/lists/1.jpg"></v-img>
                                 </v-avatar>
@@ -55,33 +38,38 @@
                         </v-row>
                     </v-card>
                 </v-col>
-                <v-col cols="6">
+                <v-col lg="6" sm="12" xs="12">
                     <v-card class="ma-4" color="grey" rounded="4" width="90%" elevation="0">
-                        <v-card-title class="my-4">Meine Autos</v-card-title>
+                        <h2 class="my-4">Meine Autos</h2>
                         <v-row>
-                            <v-col cols="4">
+                            <v-col lg="4">
                                 <v-img
                                 class="car-image my-4"
                                 aspect-ratio="1"
-                                width="60%"
                                 src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
                                 cover
                                 />
                             </v-col>
-                            <v-col cols="4">
+                            <v-col lg="4">
                                 <v-img
                                 class="car-image my-4"
                                 aspect-ratio="1"
-                                width="60%"
                                 src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
                                 cover
                                 />
                             </v-col>
-                            <v-col cols="4">
+                            <v-col lg="4">
                                 <v-img
                                 class="car-image my-4 mb-10"
                                 aspect-ratio="1"
-                                width="60%"
+                                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+                                cover
+                                />
+                            </v-col>
+                            <v-col lg="4">
+                                <v-img
+                                class="car-image my-4 mb-10"
+                                aspect-ratio="1"
                                 src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
                                 cover
                                 />
@@ -101,7 +89,7 @@
 <style scoped>
 .profile-container {
     max-width: 1300px;
-    margin-top: 9vh;
+    margin-top: 5vh;
 }
 
 .profile-pic-name {
@@ -112,5 +100,15 @@
 .car-image {
     margin: 10px;
     border-radius: 10px;
+    min-width: 100px;
+    width: 150px;
+}
+
+@media only screen and (max-width: 1279px) {
+    .profile-pic-name {
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
 }
 </style>
